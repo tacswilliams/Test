ab2c59710740f7548bc89557cb1bc43a28bbac9c
tacswilliams <55443380+tacswilliams@users.noreply.github.com>
2019-09-18 13:42:08 -0400

    Add files via upload

---
+++ 2019-09-18-Classwork/2019-09-18-Classwork/main.cpp
@@ -0,0 +1,23 @@
+// Name: Christopher Williams
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-09-18-Classwork
+
+#include <iostream>
+
+int main()
+{
+	std::cout	<< "Name: Christopher Williams" << std::endl
+				<< "------------------------------------------" << std::endl
+				<< "Period  1: Intro to Computer Science" << std::endl
+				<< "Period  2: Prep" << std::endl
+				<< "Period  3: Tech Support" << std::endl
+				<< "Period  4: AP Computer Science Principles" << std::endl
+				<< "Period  5: Prep" << std::endl
+				<< "Period  6: House" << std::endl
+				<< "Period  7: Prep" << std::endl
+				<< "Period  8: House" << std::endl
+				<< "Period  9: Tech Support" << std::endl
+				<< "Period 10: Prep" << std::endl;
+}
